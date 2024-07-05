8e7ec69d35d0a9f05316ce30eb05abd44bb1d526
Hakizimana-fred <hakifred20@gmail.com>
2024-07-05 16:54:47 +0300

    new build trigger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('express')()
 const port = 5000
 
 server.get('/', (req, res) => {
-    res.status(200).send("Hi there, welcome home! Testing out Jenkins")
+    res.status(200).send("Hi there, welcome home! Testing out Jenkins...")
 })
 
 server.listen(port, (err) => {
